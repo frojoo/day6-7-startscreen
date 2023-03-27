47b1c850cf829dc14cfa76a2e09894ceb85e840a
frojoo <wndyd29605@gmail.com>
2023-03-27 16:31:05 +0900

    add Chat GPT

--- main.js
+++ main.js
@@ -78,3 +78,54 @@ function onClickRegist() {
   quotesMsg.innerHTML = `<span>${newQuotesInput.value}</span>`; //innerText는 텍스트만 넣을 수 있는데 innerHTML은 스타일까지 먹일 수 있음
   newQuotes.style.display = "none";
 }
+
+let isLoading = false;
+
+async function onClickSearch() {
+  const searchInput = document.querySelector(".searchInput");
+  const searchResult = document.querySelector(".searchResult");
+
+  if (!searchInput.value) return; //한줄이면 {} 생략 가능
+  if (isLoading) return;
+
+  isLoading = true;
+  const question = searchInput.value;
+  searchInput.value = "검색 중 입니다... 잠시만 기다려주세요."; //question은 업데이트 못하지만 안의 값은 변경 가능
+
+  console.log("챗 지피티 동작중");
+
+  //프론트엔드에서 백엔드로 보내는 코드
+  const response = await axios.post(
+    "https://holy-fire-2749.fly.dev/chat",
+    {
+      question, //question(서버에서 설정): question(위의 searhInput.value) 같으므로 하나로 생략 가능
+    }, //{바디}
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer BLOCKCHAINSCHOOL3",
+      },
+    } // {헤더}
+  );
+
+  if (response.status === 200) {
+    searchResult.style.display = "inline";
+    searchResult.innerText = response.data.choices[0].message.content;
+  }
+
+  searchInput.value = "";
+  isLoading = false;
+}
+
+function onClickToggle(value) {
+  const nft = document.querySelector(".nft");
+  const nftView = document.querySelector(".nftView");
+
+  if (value) {
+    nft.style.display = "inline-block";
+    nftView.style.display = "none";
+  } else {
+    nft.style.display = "none";
+    nftView.style.display = "inline-block";
+  }
+}
